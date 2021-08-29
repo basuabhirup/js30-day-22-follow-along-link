ba1a1614df31430fdebc1460a08c4057571203a9
basuabhirup <basu.abhirup@yahoo.com>
2021-08-29 13:49:59 +0530

    Complete link highlighting functionality.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
+const links = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.appendChild(highlight);
 
+links.forEach(link => link.addEventListener('mouseenter', function() {
+    const linkPosition = this.getBoundingClientRect();
+    highlight.style.width = `${linkPosition.width}px`;
+    highlight.style.height = `${linkPosition.height}px`;
+    highlight.style.transform = `translate(
+      ${linkPosition.left + window.scrollX}px, ${linkPosition.top + window.scrollY}px)
+    `;
+  })
+)
